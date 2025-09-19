4a47f468adb37100076e1531503d44ac8c174068
FenLinu <2791882335@qq.com>
2025-09-19 14:31:53 +0800

    优化宽高比

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,6 +1,6 @@
-export const WINDOW_INITIAL_WIDTH = 1200
+export const WINDOW_INITIAL_WIDTH = 1500
 export const WINDOW_INITIAL_HEIGHT = 900
-export const WINDOW_MIN_WIDTH = 600
-export const WINDOW_MIN_HEIGHT = 300
+export const WINDOW_MIN_WIDTH = 800
+export const WINDOW_MIN_HEIGHT = 600
 export const USER_DATA = 'userData'
 export const CACHE = 'cache'
